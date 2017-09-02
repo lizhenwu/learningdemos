5fc8f768d4a878804be50cd8d92f4edcc7e92c97
windmill <l20122005@live.com>
2017-09-02 20:10:14 +0800

    自封装ajax

---
+++ ajax.js
@@ -0,0 +1,43 @@
+//封装一个通用的ajax方法,requirejs模块
+define(function(){
+    const myAjax = function(options,callback){
+        let xhr = request(),
+            method = options.method,
+            url = options.url,
+            data = options.data || null,
+            isAsync = options.isAsync || true;
+        if(!isAsync){
+            xhr.onreadystatechange=function()
+            {
+            if (xhr.readyState==4 && xhr.status==200)
+              {
+                  callback(xhr.responseText);
+              }
+            }
+        }
+        if(method.toLowerCase() == 'post'){
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            data = dataTransfer(data);
+        }
+        xhr.open(method,url,isAsync);
+        xhr.send(data)
+        callback(xhr.responseText)
+    }
+    const request = function(){
+        if(window.XMLHttpRequest){
+            return new XMLHttpRequest();
+        }else {
+            return new ActiveXObject('Microsoft.XMLHTTP')                                                                                                                                          
+        }
+    }
+    const dataTransfer = function(data){
+        let res = [];
+        for(let i in data){
+            res.push(`${encodeURIComponent(i)}=${encodeURIComponent(data[i])}`);
+        }
+        return res.join('&');
+    }
+    return myAjax;
+})
+
+
